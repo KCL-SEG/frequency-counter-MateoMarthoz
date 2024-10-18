c121332f8b9d10032ed6794815595599f4fabeb2
Mateo Marthoz Blanco <mateo.marthoz_blanco@kcl.ac.uk>
2024-10-18 03:57:27 +0100

    Complete frequencies function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for item in items:
+        key = str(item) # Convert the item into a string and use it as a key
+
+        if key in frequencies:
+            frequencies[key] += 1 # Increment value if the key already exists
+        else:
+            frequencies[key] = 1 # Set value to 1 if the key is new
+
     return frequencies
